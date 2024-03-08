483b583f64b03ef826ce1b0feac520489043e3f1
NovikovIlya <91413531+NovikovIlya@users.noreply.github.com>
2024-03-08 15:48:26 +0300

    03

--- src/components/Form/FormComponent.tsx
+++ src/components/Form/FormComponent.tsx
@@ -2,46 +2,59 @@ import { useEffect, useState } from 'react';
 import styles from './FormComponent.module.scss';
 import { useQuery } from '@tanstack/react-query';
 import type { ChangeEvent } from 'react';
+import axios from 'axios';
 
 import { useDebounce } from 'usehooks-ts';
 
 const FormComponent = () => {
+  // Стейты
   const [value, setValue] = useState<string>('');
-  const [double,setDouble] = useState('')
+  const [double, setDouble] = useState('');
   const debouncedValue = useDebounce<string>(value, 3000);
 
+  // Hooks
+  //// Отправка запроса
   const { refetch, data } = useQuery({
     queryKey: ['todos', value],
-    queryFn: () => fetchName(value),
+    // добавлен signal - для отмены "старых" хзапросов
+    queryFn: ({signal}) => fetchName({signal}, value),
     enabled: false,
   });
 
-  const fetchName = async (value: string) => {
+  //// Дебаунс запроса
+  useEffect(() => {
+    if (value && value !== double) {
+      refetch({ cancelRefetch: true });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedValue]);
+
+  // Функции
+  //// функция отправки
+  const fetchName = async ({ signal }: any, value: string) => {
     try {
-      const response = await fetch(`https://api.agify.io?name=${value}`);
-      const data = await response.json();
-      return data;
+      const response = await axios.get(`https://api.agify.io?name=${value}`, {
+        signal,
+      });
+      return response.data;
     } catch (error) {
       console.log(error);
     }
   };
 
+  // Изменение инпута
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
+  // Отправка запроса при клике
   const handleButton = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    refetch();
-    setDouble(value)
-  };
-
-  useEffect(() => {
-    if (value && value !== double) {
+    if (value !== double) {
       refetch();
+      setDouble(value);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedValue]);
+  };
 
   return (
     <div>
